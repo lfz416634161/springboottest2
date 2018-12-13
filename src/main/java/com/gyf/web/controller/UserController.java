a76baff2fdc399ddbe3dbb5a9f80265a9885e9c5
lfz <416634161@qq.com>
2018-12-13 14:14:24 +0800

    1.spring boot  里面的logging没用 2.只能自己在导一个log4j的包 3.拿过来记日志用

--- src/main/java/com/gyf/web/controller/UserController.java
+++ src/main/java/com/gyf/web/controller/UserController.java
@@ -2,11 +2,12 @@ package com.gyf.web.controller;
 
 
 import com.gyf.test1.service.UserServiceImpl;
-import com.gyf.test2.service.CustomerServiceImpl;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController //声明Rest风格的控制器
 //@EnableAutoConfiguration //自动配置，相当于写了spring的配置文件
@@ -14,11 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private UserServiceImpl userService;
-    @Autowired
-    private CustomerServiceImpl customerService;
+
+    Logger logger= Logger.getLogger(UserController.class);
+
     @RequestMapping("register")
     @ResponseBody
     public String register(String username,String password){
+         //记录客户端的请求参数
+        logger.info("120.1.1.0输入地址"+"username:"+username+"password:"+password);
         //把数据保存到test1数据库
         userService.register(username,password);
         return "success";
